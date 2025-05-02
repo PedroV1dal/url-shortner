4697faacf24d07f858d3e932be361ffc9e258f2e
Pedro Vidal <pedro.vidal@rentbrella.com>
2025-05-02 15:27:57 -0300

    add the logger file

---
+++ packages/auth-service/src/utils/logger.ts
@@ -0,0 +1,17 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL ?? "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+// TODO: Add transport for ELK or other service if configured
+if (process.env.ENABLE_ELK === "true") {
+  //config for ELK
+}
+
+export default logger;
